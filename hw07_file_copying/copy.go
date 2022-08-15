d0511131876f3a831a5f1d9b370322e14bf739b1
Roman Sarvarov <rasarvarov@sbercloud.ru>
2022-08-16 00:32:00 +0300

    homework 7 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"log"
+	"os"
 )
 
 var (
@@ -9,7 +14,88 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+const (
+	chunkSize int64 = 1
+)
+
+func Copy(fromPath, toPath string, offset, limit int64) (err error) {
+	in, err := os.Open(fromPath)
+	if err != nil {
+		return wrapErr(err)
+	}
+	defer doClose(in)
+
+	f, err := in.Stat()
+	if err != nil {
+		return wrapErr(err)
+	}
+
+	fromSize := f.Size()
+
+	if fromSize == 0 {
+		return ErrUnsupportedFile
+	}
+
+	if offset > fromSize {
+		return ErrOffsetExceedsFileSize
+	}
+
+	out, err := os.Create(toPath)
+	if err != nil {
+		return wrapErr(err)
+	}
+	defer doClose(out)
+
+	if _, err = in.Seek(offset, io.SeekStart); err != nil {
+		log.Fatal(err)
+	}
+
+	total := calculateTotal(fromSize, offset)
+
+	if limit == 0 {
+		limit = total
+	}
+
+	bar := pb.StartNew(int(limit))
+
+	for {
+		size := chunkSize
+		if size > limit {
+			size = limit
+		}
+
+		n, err := io.CopyN(out, in, size)
+
+		eof := err == io.EOF
+
+		if err != nil && !eof {
+			return wrapErr(err)
+		}
+
+		bar.Add(int(n))
+		limit -= n
+
+		if eof || limit <= 0 {
+			break
+		}
+	}
+
+	bar.Finish()
+
 	return nil
+}
+
+func calculateTotal(size int64, offset int64) int64 {
+	return size - offset
+}
+
+func wrapErr(err error) error {
+	return fmt.Errorf("error while copy file: %w", err)
+}
+
+func doClose(closer io.Closer) {
+	err := closer.Close()
+	if err != nil {
+		log.Println("error while close: ", err)
+	}
 }
